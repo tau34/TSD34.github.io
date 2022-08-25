264eb4868cbeab56fb66258820c4305f478d7699
TSD34 <109936561+TSD34@users.noreply.github.com>
2022-08-25 19:05:51 +0900

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-function select() {
-    var t = document.getElementById("input").value;
-    document.getElementById("0").style.display = "none"
-    document.getElementById("1").style.display = "none"
-    document.getElementById("2").style.display = "none"
-    document.getElementById("3").style.display = "none"
-    document.getElementById(t).style.display = "block"
-    setTimeout(select, 20)
-}
